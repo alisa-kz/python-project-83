68d294d064aed1831ad4e5e1183f5f211516d1fd
Alisa Kuzyagina <khairutdinovaaalisa@gmail.com>
2024-12-19 23:12:18 +0300

    add basic functionality

--- Makefile
+++ Makefile
@@ -6,12 +6,12 @@ install:
 dev:
 	poetry run flask --app page_analyzer:app run
 
-build:
-	poetry build
-
 lint:
 	poetry run flake8 page_analyzer
 
 PORT ?= 8000
 start:
 	poetry run gunicorn -w 5 -b 0.0.0.0:$(PORT) page_analyzer:app
+
+build:
+	./build.sh
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,13 +1,100 @@
+import psycopg2
 import os
+import validators
 
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for, get_flashed_messages
+from datetime import datetime
+from urllib.parse import urlparse
+from psycopg2.extras import DictCursor
+
 
 load_dotenv()
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+DATABASE_URL = os.getenv("DATABASE_URL")
+conn = psycopg2.connect(DATABASE_URL)
 
 
 @app.route("/")
 def main():
-    return render_template("cover/index.html")
+    return render_template("index.html")
+
+
+def validate(url):
+    errors = {}
+    if len(url) > 255:
+        errors["big_len"] = "Адрес сайта не должен превышать 255 символов"
+    if not validators.url(url):
+        errors["not_valid"] = "Неправильный адрес"
+    return errors
+
+
+@app.post("/urls")
+def get_url():
+    url_dict = request.form.to_dict()
+    url_full = url_dict['url']
+    errors = validate(url_full)
+    if errors:
+        flash("Некорректный URL", "alert alert-danger")
+        messages = get_flashed_messages(with_categories=True)
+        return (
+            render_template(
+                "index.html",
+                url=url_full,
+                errors=errors,
+                messages=messages
+            ),
+            422,
+        )
+    url_norm = urlparse(url_full)
+    url = url_norm.scheme + "://" + url_norm.hostname
+    now_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    sql = "INSERT INTO urls (name, created_at) VALUES (%s, %s)"
+    sql_select = "SELECT * FROM urls WHERE name=%s"
+    sql_id = "SELECT id FROM urls WHERE name=%s"
+    with conn:
+        with conn.cursor() as curs:
+            curs.execute(sql_select, (url, ))
+            url_ex = curs.fetchone()
+            if url_ex:
+                flash("Страница уже существует", "alert alert-info")
+            else:
+                curs.execute(sql, (url, now_date))
+                flash("Страница успешно добавлена", "alert alert-success")
+            curs.execute(sql_id, (url, ))
+            url_id = curs.fetchone()[0]
+        conn.commit()
+    return redirect(url_for("show_url", id=url_id), code=302)
+
+
+@app.route("/urls/<id>")
+def show_url(id):
+    sql = "SELECT * FROM urls WHERE id=%s"
+    with conn:
+        with conn.cursor(cursor_factory=DictCursor) as curs:
+            curs.execute(sql, (id,))
+            url = curs.fetchone()
+        conn.commit()
+    messages = get_flashed_messages(with_categories=True)
+    return render_template(
+        "show.html",
+        url=url,
+        messages=messages
+    )
+
+
+@app.route("/urls")
+def show_all_urls():
+    sql = "SELECT * FROM urls ORDER BY id DESC"
+    with conn:
+        with conn.cursor(cursor_factory=DictCursor) as curs:
+            curs.execute(sql)
+            urls = curs.fetchall()
+        conn.commit()
+    messages = get_flashed_messages(with_categories=True)
+    return render_template(
+        "urls.html",
+        urls=urls,
+        messages=messages
+    )
