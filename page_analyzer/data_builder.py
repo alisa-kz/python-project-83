ef5dfb3d76b16a2a969654e2cbd01cfe38e8fb3d
Alisa Kuzyagina <khairutdinovaaalisa@gmail.com>
2025-01-03 13:55:02 +0300

    update data_builder.py

--- page_analyzer/data_builder.py
+++ page_analyzer/data_builder.py
@@ -4,23 +4,17 @@ from bs4 import BeautifulSoup
 
 
 def data_build(response, id):
-    check_data = {}
-    check_data['url_id'] = id
-    code = response.status_code
-    check_data['code'] = code
     bs = BeautifulSoup(response.text, 'html.parser')
-    h1 = bs.h1.string
-    check_data['h1'] = h1
-    title = bs.title.string
-    check_data['title'] = title
-    metas = bs.find_all('meta')
-    for meta in metas:
-        if meta.get('name') == 'description':
-            content = meta['content']
-            check_data['content'] = content
-            break
-        else:
-            check_data['content'] = None
-    ch_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    check_data['ch_date'] = ch_date
+
+    check_data = {
+        'url_id': id,
+        'code': response.status_code,
+        'h1': bs.h1.string if bs.h1 else None,
+        'title': bs.title.string if bs.title else None,
+        'content': None,
+        'ch_date': datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+    description_meta = bs.find('meta', {"name": 'description'}).get('content')
+    if description_meta:
+        check_data['content'] = description_meta
     return check_data
